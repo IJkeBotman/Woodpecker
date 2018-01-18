8f6e514b840f7a0dfb65433c3b58c70f66973b97
IJke <ijke@overscope.net>
2018-01-18 22:21:51 +0100

    Missing AppDelegate

---
+++ Woodpecker/Classes/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Woodpecker
+//
+//  Created by IJke Botman on 18/01/2018.
+//  Copyright © 2018 IJke Botman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+}
+
